d8b2fc4063cd57af40ce4cc0bd082059cb71f335
Nidhan <mail2nidhan@gmail.com>
2024-06-26 23:21:31 +0530

    updated user-views

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,45 +1,43 @@
-var db=require('../config/connection')
-var collection=require('../config/collections')
-const bcrypt=require('bcrypt')
-module.exports={
-    doSignup:(userData)=>{
-        console.log(userData);
-        return new Promise(async(resolve,reject)=>{
-            userData.Password=await bcrypt.hash(userData.Password,10)
-            db.get().collection(collection.USER_COLLECTION).insertOne(userData).then((data)=>{
-                resolve(data.insertedId)
-
-            })
-        })
-
-
-    },
-    doLogin:userData=>{
-        return new Promise(async(resolve,reject)=>{
-            let loginstatus=false
-            let response={}
-            let user=await db.get().collection(collection.USER_COLLECTION).findOne({Email:userData.Email})
-            console.log(user);
-            if(user){
-                bcrypt.compare(userData.Password,user.Password).then((status)=>{
-                    if(status){
-                        console.log('login success');
-                        response.user=user
-                        response.status=true
-                        resolve(response)
- 
-                    }else{
-                        console.log('Login failed');
-                        resolve({status:false})
-                    }
-
-
-                })
-
-            }else{
-                console.log("login failed");
-            }
-
-        })
-    }
-}
+var db = require("../config/connection");
+var collection = require("../config/collections");
+const bcrypt = require("bcrypt");
+module.exports = {
+  doSignup: (userData) => {
+    console.log(userData);
+    return new Promise(async (resolve, reject) => {
+      userData.Password = await bcrypt.hash(userData.Password, 10);
+      db.get()
+        .collection(collection.USER_COLLECTION)
+        .insertOne(userData)
+        .then((data) => {
+          resolve(data.insertedId);
+        });
+    });
+  },
+  doLogin: (userData) => {
+    return new Promise(async (resolve, reject) => {
+      let loginstatus = false;
+      let response = {};
+      let user = await db
+        .get()
+        .collection(collection.USER_COLLECTION)
+        .findOne({ Email: userData.Email });
+      console.log(user);
+      if (user) {
+        bcrypt.compare(userData.Password, user.Password).then((status) => {
+          if (status) {
+            console.log("login success");
+            response.user = user;
+            response.status = true;
+            resolve(response);
+          } else {
+            console.log("Login failed");
+            resolve({ status: false });
+          }
+        });
+      } else {
+        console.log("login failed");
+      }
+    });
+  },
+};
--- routes/users.js
+++ routes/users.js
@@ -1,72 +1,32 @@
-var express = require('express');
-const productHelpers = require('../helpers/product-helpers');
+var express = require("express");
+const productHelpers = require("../helpers/product-helpers");
 var router = express.Router();
-const userHelpers=require('../helpers/user-helpers')
-const verifyLogin=(req,res,next)=>{
-  if(req.session.loggedIn){
-    next()
-  }else{
-    res.redirect('/login')
+const userHelpers = require("../helpers/user-helpers");
+const {
+  homepage,
+  loginpage,
+  signuppage,
+  signuppost,
+  loginpost,
+  logout,
+  cart,
+} = require("../controllers/user-controller");
+const verifyLogin = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect("/login");
   }
-}
-
+};
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  let user=req.session.user
+router.get("/", homepage);
 
-
-  productHelpers.getAllProducts().then((products)=>{
-    res.render('user/view-products',{admin:false,products,user});
-
-  
-
-  
-  })
-
- 
-});
-router.get('/login',(req,res)=>{
-  if(req.session.loggedIn){
-    res.redirect('/')
-  }else{
-
-    res.render('user/login',{"loginErr":req.session.loginErr})
-    req.session.loginErr=false
-  }
-})
-router.get('/signup',(req,res)=>{
-  res.render('user/signup')
-})
-router.post('/signup',(req,res)=>{
-  console.log(req.body);
-  userHelpers.doSignup(req.body).then((response)=>{
-    console.log(response);
-  })
-
-})
-router.post('/login',(req,res)=>{
-  userHelpers.doLogin(req.body).then((response)=>{
-    if(response.status){
-      console.log(response.status);
-      req.session.loggedIn=true
-      req.session.user=response.user
-      res.redirect('/')
-    }else{
-      req.session.loginErr="Invalid username or password"
-      res.redirect('/login')
-    }
-  })
-
-})
-router.get('/logout',(req,res)=>{
-  req.session.destroy()
-  res.redirect('/')
-
-})
-router.get('/cart',verifyLogin,(req,res)=>{
-  res.render('user/cart')
-})
-
+router.get("/login", loginpage);
+router.get("/signup", signuppage);
+router.post("/signup", signuppost);
+router.post("/login", loginpost);
+router.get("/logout", logout);
+router.get("/cart", verifyLogin, cart);
 
 module.exports = router;
