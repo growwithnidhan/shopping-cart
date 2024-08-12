e571592a0c67bec26d2ecb2adc49eec966f6dbde
Nidhan <mail2nidhan@gmail.com>
2024-08-12 09:04:11 +0530

    fetched data of all orders to admin

--- helpers/admin-helpers.js
+++ helpers/admin-helpers.js
@@ -1,12 +1,14 @@
+const db = require("../config/connection");
 const bcrypt = require("bcrypt");
+const collections = require("../config/collections");
+const { ObjectId } = require("mongodb");
 
 // Define admin credentials
 const adminCredentials = {
   email: "admin@gmail.com",
-  password: "" // Will be replaced with hashed password
+  password: "",
 };
 
-// Hash the admin password and store it in adminCredentials
 bcrypt.hash("admin", 10).then((hash) => {
   adminCredentials.password = hash;
 });
@@ -16,23 +18,38 @@ module.exports = {
     return new Promise((resolve, reject) => {
       let response = {};
       if (admindata.email === adminCredentials.email) {
-      console.log()
-        bcrypt.compare(admindata.password, adminCredentials.password).then((status) => {
-          if (status) {
-            console.log("admin login successful");
-            response.admin = adminCredentials;
-            response.status = true;
-            resolve(response);
-          } else {
-            console.log("admin login failed");
-            response.status = false;
-            resolve(response);
-          }
-        });
+        console.log();
+        bcrypt
+          .compare(admindata.password, adminCredentials.password)
+          .then((status) => {
+            if (status) {
+              console.log("admin login successful");
+              response.admin = adminCredentials;
+              response.status = true;
+              resolve(response);
+            } else {
+              console.log("admin login failed");
+              response.status = false;
+              resolve(response);
+            }
+          });
       } else {
         console.log("ithil admin login failed");
         response.status = false;
         resolve(response);
+      }
+    });
+  },
+  getAllOrders: () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const orders = await db.get()
+          .collection(collections.ORDER_COLLECTION)
+          .find()
+          .toArray();
+        resolve(orders);
+      } catch (error) {
+        reject(error);
       }
     });
   },
